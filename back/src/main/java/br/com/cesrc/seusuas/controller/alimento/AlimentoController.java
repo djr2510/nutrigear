21108474009c6dddc15acc4dcdd6ab8d0e02d7c1
Davi Jaques <davi251006@gmail.com>
2025-01-20 21:03:26 -0300

    Alimento sendo feito

--- back/src/main/java/br/com/cesrc/seusuas/controller/alimento/AlimentoController.java
+++ back/src/main/java/br/com/cesrc/seusuas/controller/alimento/AlimentoController.java
@@ -1,13 +1,9 @@
 package br.com.cesrc.seusuas.controller.alimento;
 
 import br.com.cesrc.seusuas.controller.alimento.response.AlimentoResponse;
-import br.com.cesrc.seusuas.controller.pessoa.response.PessoaResponse;
 import br.com.cesrc.seusuas.model.alimento.AlimentoModel;
-import br.com.cesrc.seusuas.model.pessoa.PessoaModel;
+import br.com.cesrc.seusuas.model.alimento.TipoAlimento;
 import br.com.cesrc.seusuas.repository.AlimentoRepository;
-import br.com.cesrc.seusuas.repository.PessoaRepository;
-import br.com.cesrc.seusuas.service.AlimentoService;
-import br.com.cesrc.seusuas.service.PessoaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,35 +12,112 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping(path="/alimento")
+@RequestMapping(path = "/alimento")
 @RestController
 public class AlimentoController {
 
-private AlimentoService alimentoService;
-private AlimentoRepository alimentoRepository;
+    private final AlimentoRepository alimentoRepository;
 
-    public AlimentoController (AlimentoService alimentoService, AlimentoRepository alimentoRepository) {
-        this.alimentoService = alimentoService;
+    public AlimentoController(AlimentoRepository alimentoRepository) {
         this.alimentoRepository = alimentoRepository;
     }
 
     @GetMapping
-    public ResponseEntity<List<AlimentoResponse>> listarAlimentos(@RequestParam(required = false)Long id){
-
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentos() {
         List<AlimentoModel> alimentos = alimentoRepository.findAll();
         List<AlimentoResponse> responseList = alimentos.stream()
                 .map(AlimentoResponse::of)
                 .toList();
         return ResponseEntity.ok(responseList);
     }
-    @GetMapping
-    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorNome(@RequestParam(required = false)String nome){
 
+    // Retorna alimentos filtrados pelo nome
+    @GetMapping("/buscar-por-nome")
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorNome(@RequestParam String nome) {
         List<AlimentoModel> alimentos = alimentoRepository.findByNomeContaining(nome);
         List<AlimentoResponse> responseList = alimentos.stream()
                 .map(AlimentoResponse::of)
                 .toList();
         return ResponseEntity.ok(responseList);
     }
-}
 
+    // Ordena alimentos pela vitamina A (decrescente)
+    @GetMapping("/ordenar-por-vitaminaA")
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorVitaminaADesc() {
+        List<AlimentoModel> alimentos = alimentoRepository.findAllOrderByVitaminaADesc();
+        List<AlimentoResponse> responseList = alimentos.stream()
+                .map(AlimentoResponse::of)
+                .toList();
+        return ResponseEntity.ok(responseList);
+    }
+
+    // Ordena alimentos pela vitamina B (decrescente)
+    @GetMapping("/ordenar-por-vitaminaB")
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorVitaminaBDesc() {
+        List<AlimentoModel> alimentos = alimentoRepository.findAllOrderByVitaminaBDesc();
+        List<AlimentoResponse> responseList = alimentos.stream()
+                .map(AlimentoResponse::of)
+                .toList();
+        return ResponseEntity.ok(responseList);
+    }
+
+    // Ordena alimentos pela vitamina C (decrescente)
+    @GetMapping("/ordenar-por-vitaminaC")
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorVitaminaCDesc() {
+        List<AlimentoModel> alimentos = alimentoRepository.findAllOrderByVitaminaCDesc();
+        List<AlimentoResponse> responseList = alimentos.stream()
+                .map(AlimentoResponse::of)
+                .toList();
+        return ResponseEntity.ok(responseList);
+    }
+
+    // Ordena alimentos pela vitamina D (decrescente)
+    @GetMapping("/ordenar-por-vitaminaD")
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorVitaminaDDesc() {
+        List<AlimentoModel> alimentos = alimentoRepository.findAllOrderByVitaminaDDesc();
+        List<AlimentoResponse> responseList = alimentos.stream()
+                .map(AlimentoResponse::of)
+                .toList();
+        return ResponseEntity.ok(responseList);
+    }
+
+    // Ordena alimentos pela vitamina E (decrescente)
+    @GetMapping("/ordenar-por-vitaminaE")
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorVitaminaEDesc() {
+        List<AlimentoModel> alimentos = alimentoRepository.findAllOrderByVitaminaEDesc();
+        List<AlimentoResponse> responseList = alimentos.stream()
+                .map(AlimentoResponse::of)
+                .toList();
+        return ResponseEntity.ok(responseList);
+    }
+
+    // Ordena alimentos pela vitamina K (decrescente)
+    @GetMapping("/ordenar-por-vitaminaK")
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorVitaminaKDesc() {
+        List<AlimentoModel> alimentos = alimentoRepository.findAllOrderByVitaminaKDesc();
+        List<AlimentoResponse> responseList = alimentos.stream()
+                .map(AlimentoResponse::of)
+                .toList();
+        return ResponseEntity.ok(responseList);
+    }
+
+    // Retorna alimentos de um tipo específico
+    @GetMapping("/buscar-por-tipo")
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorTipo(@RequestParam TipoAlimento tipoAlimento) {
+        List<AlimentoModel> alimentos = alimentoRepository.findByTipo(tipoAlimento);
+        List<AlimentoResponse> responseList = alimentos.stream()
+                .map(AlimentoResponse::of)
+                .toList();
+        return ResponseEntity.ok(responseList);
+    }
+
+    // Ordena alimentos pela quantidade de proteínas (decrescente)
+    @GetMapping("/ordenar-por-proteinas")
+    public ResponseEntity<List<AlimentoResponse>> listarAlimentosPorProteinasDesc() {
+        List<AlimentoModel> alimentos = alimentoRepository.findAllOrderByProteinasDesc();
+        List<AlimentoResponse> responseList = alimentos.stream()
+                .map(AlimentoResponse::of)
+                .toList();
+        return ResponseEntity.ok(responseList);
+    }
+}
--- back/src/main/java/br/com/cesrc/seusuas/service/AlimentoService.java
+++ back/src/main/java/br/com/cesrc/seusuas/service/AlimentoService.java
@@ -1,9 +1,8 @@
 package br.com.cesrc.seusuas.service;
 
 import br.com.cesrc.seusuas.model.alimento.AlimentoModel;
-import br.com.cesrc.seusuas.model.pessoa.PessoaModel;
+import br.com.cesrc.seusuas.model.alimento.TipoAlimento;
 import br.com.cesrc.seusuas.repository.AlimentoRepository;
-import br.com.cesrc.seusuas.repository.PessoaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,13 +11,56 @@ import java.util.List;
 public class AlimentoService {
 
     private final AlimentoRepository alimentoRepository;
-    public AlimentoService(AlimentoRepository alimentoRepository){this.alimentoRepository = alimentoRepository;}
-    public AlimentoModel salvarAlimento (AlimentoModel alimento){return alimentoRepository.save(alimento);}
-    public void deletarAlimento (Long id){
+
+    public AlimentoService(AlimentoRepository alimentoRepository) {
+        this.alimentoRepository = alimentoRepository;
+    }
+
+    public AlimentoModel salvarAlimento(AlimentoModel alimento) {
+        return alimentoRepository.save(alimento);
+    }
+
+    public void deletarAlimento(Long id) {
         alimentoRepository.deleteById(id);
     }
-    public List<AlimentoModel> listarAlimento(){
+
+    public List<AlimentoModel> listarAlimento() {
         return alimentoRepository.findAll();
     }
-    public List<AlimentoModel> ListarAlimentoPorNome(String nome){return alimentoRepository.findByNomeContaining(nome);}
+
+    public List<AlimentoModel> listarAlimentoPorNome(String nome) {
+        return alimentoRepository.findByNomeContaining(nome);
+    }
+
+    public List<AlimentoModel> listarAlimentosPorVitaminaADesc() {
+        return alimentoRepository.findAllOrderByVitaminaADesc();
+    }
+
+    public List<AlimentoModel> listarAlimentosPorVitaminaBDesc() {
+        return alimentoRepository.findAllOrderByVitaminaBDesc();
+    }
+
+    public List<AlimentoModel> listarAlimentosPorVitaminaCDesc() {
+        return alimentoRepository.findAllOrderByVitaminaCDesc();
+    }
+
+    public List<AlimentoModel> listarAlimentosPorVitaminaDDesc() {
+        return alimentoRepository.findAllOrderByVitaminaDDesc();
+    }
+
+    public List<AlimentoModel> listarAlimentosPorVitaminaEDesc() {
+        return alimentoRepository.findAllOrderByVitaminaEDesc();
+    }
+
+    public List<AlimentoModel> listarAlimentosPorVitaminaKDesc() {
+        return alimentoRepository.findAllOrderByVitaminaKDesc();
+    }
+
+    public List<AlimentoModel> listarAlimentosPorTipo(TipoAlimento tipoAlimento) {
+        return alimentoRepository.findByTipo(tipoAlimento);
+    }
+
+    public List<AlimentoModel> listarAlimentosPorProteinasDesc() {
+        return alimentoRepository.findAllOrderByProteinasDesc();
+    }
 }
