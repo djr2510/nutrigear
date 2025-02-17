c2c0fc450eeb14bc5d2d4a0cdd48da591337c54d
gabriel <guzzy04@hotmail.com>
2025-02-17 20:29:44 -0300

    comentário service adicionado

---
+++ back/src/main/java/br/com/cesrc/seusuas/service/ComentarioService.java
@@ -0,0 +1,26 @@
+package br.com.cesrc.seusuas.service;
+import br.com.cesrc.seusuas.model.comentario.ComentarioModel;
+import br.com.cesrc.seusuas.repository.ComentarioRepository;
+
+import java.util.List;
+
+public class ComentarioService {
+
+    private final ComentarioRepository comentarioRepository;
+
+    public ComentarioService(ComentarioRepository comentarioRepository) {
+        this.comentarioRepository = comentarioRepository;
+    }
+
+    public ComentarioModel salvarComentario(ComentarioModel comentario) {
+        return comentarioRepository.save(comentario);
+    }
+
+    public void deletarComentario(Long id) {
+        comentarioRepository.deleteById(id);
+    }
+
+    public List<ComentarioModel> listarComentario() {
+        return comentarioRepository.findAll();
+    }
+}
