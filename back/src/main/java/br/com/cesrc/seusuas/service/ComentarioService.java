b7b4a2cbdb22434d4c9257b1f436ac7e0f8d4a62
gabriel <guzzy04@hotmail.com>
2025-02-17 20:33:59 -0300

    comentário service adicionado

--- back/src/main/java/br/com/cesrc/seusuas/service/ComentarioService.java
+++ back/src/main/java/br/com/cesrc/seusuas/service/ComentarioService.java
@@ -16,6 +16,7 @@ public class ComentarioService {
         return comentarioRepository.save(comentario);
     }
 
+
     public void deletarComentario(Long id) {
         comentarioRepository.deleteById(id);
     }
