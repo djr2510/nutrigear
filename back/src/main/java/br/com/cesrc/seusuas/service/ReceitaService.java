6cb7a7cb97e5e333318112e5523d391b2bc6a004
gabriel <guzzy04@hotmail.com>
2025-02-17 21:07:20 -0300

    receita service adicionado

---
+++ back/src/main/java/br/com/cesrc/seusuas/service/ReceitaService.java
@@ -0,0 +1,35 @@
+package br.com.cesrc.seusuas.service;
+
+import br.com.cesrc.seusuas.model.receita.ReceitaModel;
+import br.com.cesrc.seusuas.repository.ReceitaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ReceitaService {
+
+    private final ReceitaRepository receitaRepository;
+
+    @Autowired
+    public ReceitaService(ReceitaRepository receitaRepository) { this.receitaRepository = receitaRepository;}
+    public ReceitaModel salvarReceita(ReceitaModel receita) {
+        return receitaRepository.save(receita);
+    }
+
+    public List<ReceitaModel> buscarPorTitulo(String titulo) {
+        return receitaRepository.findByNomeContaining(titulo);
+    }
+
+    public List<ReceitaModel> buscarTodasOrdenadasPorDataAsc() {
+        return receitaRepository.findAllOrderBydataCriacaoAsc();
+    }
+
+    public List<ReceitaModel>  buscarTodasOrdenadasPorDataDesc(){
+        return receitaRepository.findAllOrderBydataCriacaoDesc();
+    }
+
+    public Optional<ReceitaModel> buscarPorId(Long id) {
+        return receitaRepository.findById(id);
+    }
+}
